5a0bbb1f4a0bbc7481e1528b3b5dc982029457ba
telechubby <88198476+telechubby@users.noreply.github.com>
2022-12-19 23:17:11 +0100

    Zadaca - Nedela 2 - Resenie

--- src/Zadaca4.java
+++ src/Zadaca4.java
@@ -36,28 +36,73 @@ public class Zadaca4 {
     public static DLL<String> cities=new DLL<>();
     public static void insert_city(String name, int position){
         //TODO: your code here
-
+        System.out.println(cities.length());
+        if(position<=0)
+            cities.insertFirst(name);
+        else if(position>= cities.length()) {
+            cities.insertLast(name);
+        }
+        else{
+            DLLNode<String> current=cities.getFirst();
+            for(int i=0;i<position;i++)
+                current=current.succ;
+            cities.insertBefore(name,current);
+        }
     }
 
     public static void remove_city(int position){
         //TODO: your code here
-
+        if(position<=0)
+            cities.deleteFirst();
+        else if(position>= cities.length()-1)
+            cities.deleteLast();
+        else{
+            DLLNode<String> current=cities.getFirst();
+            for(int i=0;i<position;i++)
+                current=current.succ;
+            cities.delete(current);
+        }
     }
 
     public static void swap_cities(int position1, int position2){
         //TODO: your code here
-
+        if(position1<0)
+            position1=0;
+        if(position2<0)
+            position2=0;
+        if(position1> cities.length()-1)
+            position1= cities.length()-1;
+        if(position2> cities.length()-1)
+            position2= cities.length()-1;
+        DLLNode<String> current1=cities.getFirst();
+        DLLNode<String> current2=cities.getFirst();
+        for(int i=0;i<position1;i++)
+            current1=current1.succ;
+        for(int i=0;i<position2;i++)
+            current2=current2.succ;
+        String tmp=current1.element;
+        current1.element= current2.element;
+        current2.element=tmp;
     }
 
     public static void reverse_trip() {
         //TODO: your code here
-
+        DLLNode<String> current1=cities.getFirst();
+        DLLNode<String> current2=cities.getLast();
+        int length= cities.length();
+        for(int i=0;i<length/2;i++){
+            String tmp=current1.element;
+            current1.element= current2.element;
+            current2.element=tmp;
+            current1=current1.succ;
+            current2=current2.pred;
+        }
     }
 
     public static void main(String[] args) {
-        cities.insertLast("Tokyo");
-        cities.insertLast("Barcelona");
-        cities.insertLast("Copenhagen");
+        cities.insertLast("0");
+        cities.insertLast("1");
+        cities.insertLast("2");
         Scanner scanner=new Scanner(System.in);
         int n=-1;
         while(n!=5){
@@ -72,14 +117,16 @@ public class Zadaca4 {
                 case 1 -> {
                     System.out.print("Please enter the city name: ");
                     String name = scanner.nextLine();
-                    System.out.printf("Please enter the position of the city in the list (from 0 to %d): ",cities.length()-1);
+                    System.out.printf("Please enter the position of the city in the list (from 0 to %d): ",cities.length());
                     int position = Integer.parseInt(scanner.nextLine());
                     insert_city(name, position);
+                    System.out.println(cities);
                 }
                 case 2 -> {
                     System.out.printf("Please enter the position of the city in the list (from 0 to %d): ",cities.length()-1);
                     int position = Integer.parseInt(scanner.nextLine());
                     remove_city(position);
+                    System.out.println(cities);
                 }
                 case 3 -> {
                     System.out.printf("Please enter the position of the first city in the list (from 0 to %d): ",cities.length()-1);
@@ -87,8 +134,12 @@ public class Zadaca4 {
                     System.out.printf("Please enter the position of the second city in the list (from 0 to %d): ",cities.length()-1);
                     int position2 = Integer.parseInt(scanner.nextLine());
                     swap_cities(position1, position2);
+                    System.out.println(cities);
                 }
-                case 4 -> reverse_trip();
+                case 4 -> {
+                    reverse_trip();
+                    System.out.println(cities);
+                }
                 case 5 -> {
                     break;
                 }
